514be272d73b58c6dbca6f0ee70bb43a8436a894
Lavictus <markbaaij@lavictus.nl>
2020-01-13 18:53:32 +0100

    Auto zoom to location

--- app/src/main/java/com/example/luckoftheirish/MapsActivity.java
+++ app/src/main/java/com/example/luckoftheirish/MapsActivity.java
@@ -1,25 +1,34 @@
 package com.example.luckoftheirish;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private final int DEFAULT_ZOOM = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -39,8 +49,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     0
                     );
             mMap.setMyLocationEnabled(true);
+            getDeviceLocation();
         } else {
             mMap.setMyLocationEnabled(true);
+            getDeviceLocation();
         }
+
+
+    }
+
+    public void getDeviceLocation(){
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        0
+                );
+                getDeviceLocation();
+            }else{
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if(task.isSuccessful()){
+                            Location location = task.getResult();
+                            LatLng currentPos = new LatLng(location.getLatitude(), location.getLongitude());
+                            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(currentPos, DEFAULT_ZOOM);
+                            mMap.moveCamera(update);
+                        }
+                    }
+                });
+            }
     }
 }
