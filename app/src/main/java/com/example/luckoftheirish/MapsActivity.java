e722abe5e827d9466a2f30cb87b89461217993dd
Luke Taylor <luketaylor001@gmail.com>
2020-01-13 16:36:06 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/luckoftheirish/MapsActivity.java
+++ app/src/main/java/com/example/luckoftheirish/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.example.luckoftheirish;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,20 +30,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+<<<<<<< Updated upstream
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
@@ -52,5 +47,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.addMarker(new MarkerOptions().position(breda).title("Marker in Breda"));
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+=======
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    0
+                    );
+            mMap.setMyLocationEnabled(true);
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
+
+>>>>>>> Stashed changes
     }
 }
